7bdeff9ee91f8c6ad5b75bd56f52d546bca90222
UsmanUmer-wq <57863260+UsmanUmer-wq@users.noreply.github.com>
2019-11-18 20:41:35 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+#This is a metadata file, to elaborate on different details of a song.
+genre="rap"
+artist="young stunners"
+relese="August 2019"
+views="500000"
+likes="499000"
+duration="2:09"
+revenue="1.9000000"
+album="rebirth"
+
+print(genre)
+print(artist)
+print(release)
+print(views)
+print(likes)
+print(duration)
+print(revenue)
+print(album)
+
+''''
+genre means the type of song this is
+artist is the person who created this song or sang this song
+release is the date when this song was first publically published
+views are the number of people who have heard this song
+likes are the number of people who gave this video a heart on spotify
+duration is the length of this song 
+revenue is the amount of money it has generated 
+Album is like a chapter containing a sub chapter which is like this case.Its basically a song 
+which is contained within an album.
