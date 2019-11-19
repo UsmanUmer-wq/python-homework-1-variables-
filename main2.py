b240d746590da9d8081616354df62839a5b07fb5
UsmanUmer-wq <57863260+UsmanUmer-wq@users.noreply.github.com>
2019-11-19 20:28:53 +0500

    Add files via upload
    homework #2 (functions)

---
+++ main2.py
@@ -0,0 +1,26 @@
+def genre():
+	print("rap")
+def artist():
+	print("youngstunners")
+def year():
+	print("2019")
+
+genre()
+artist()
+year()
+
+def verified():
+	a=True
+	print(a)
+	return a
+
+verified()
+
+
+def nonverified():
+
+	b=False
+	print(b)
+	return b
+
+nonverified()
